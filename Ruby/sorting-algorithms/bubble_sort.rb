d5ace1a231ca48d2cdb9635020cf81abb68086bb
Esther Leytush <eleytush@gmail.com>
2018-09-17 00:12:35 -0400

    update some sorting algo syntax

--- Ruby/sorting-algorithms/bubble_sort.rb
+++ Ruby/sorting-algorithms/bubble_sort.rb
@@ -1,22 +1,23 @@
 def bubble_sort(array)
-    sorted = false
-    while sorted == false
-        array.each_index do |i|
-            a = array[i]
-            b = array[i + 1]
-            
-            if b == nil
-                sorted = true
-                break
-            elsif a > b 
-                sorted = false
-                array[i], array[i + 1] = array[i + 1], array[i]
-                break
-            end
-        end
+  sorted = false
+  while sorted == false
+    array.each_index do |i|
+      a = array[i]
+      b = array[i + 1]
+
+      if b == nil
+        sorted = true
+        break
+      elsif a > b
+        sorted = false
+        array[i], array[i + 1] = array[i + 1], array[i]
+        break
+      end
     end
-    p array
+  end
+
+  p array
 end
 
 
-bubble_sort([4,3,6, 11, 9])
+bubble_sort([4, 3, 6, 11, 9])
